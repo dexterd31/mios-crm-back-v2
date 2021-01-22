4103d333fad809e029d0796d2095b4141bf432bf
Carlos <cgalindez12@gail.com>
2021-01-22 10:05:57 -0500

    Implementacion de Trail
    Se implementa trail para el manejo de respuestas al front, este debe ser usado en todos los controllers 
    ExampleController tiene el ejemplo de como usar

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Traits\ApiResponse;
 use Laravel\Lumen\Routing\Controller as BaseController;
 
 class Controller extends BaseController
--- app/Http/Controllers/ExampleController.php
+++ app/Http/Controllers/ExampleController.php
@@ -13,6 +13,20 @@ class ExampleController extends Controller
     {
         //
     }
+       //ModelTest no existe, es solo para ejemplo,
+    //la data debe ser retornada con $this->successResponse() cuando sea 200 
+    // y $this->errorResponse() cuando sea un error, este recibira dos parametros, mensaje de error y codigo de error
+    public function index()
+    {
+        $orders = ModelTest::all();
+
+        //Return 200
+        return $this->successResponse($orders);
+
+        //Return 404 
+        return $this->errorResponse('No hay ordenes disponibles',404);
+    }
+    //
 
     //
 }
