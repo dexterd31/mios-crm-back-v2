7d9c81759c0ac4f6f7d8b5b72ca461e633047255
nicoll.ramirez <nicoll.r@montechelo.com.co>
2021-02-25 17:01:23 -0500

    guardar informacion del formulario terminada

--- app/Http/Controllers/FormAnswerController.php
+++ app/Http/Controllers/FormAnswerController.php
@@ -17,82 +17,121 @@ class FormAnswerController extends Controller
      */
     public function saveinfo(Request $request)
     {
-        $json_body = json_decode($request->getContent());
-        $client = null; 
-        if($json_body->client_id == null){
-           
-            foreach($json_body->sections as $section)
-            {
-                if(!empty($section->document_type_id)){
-                    $client = new Client([
-                        'document_type_id' => $section->document_type_id,
-                        'first_name' => $section->firstName,
-                        'middle_name' => $section->middleName,
-                        'first_lastname' => $section->lastName,
-                        'second_lastname' => $section->secondLastName,
-                        'document' => $section->document
-                    ]);
-                    $client->save();
-                }else{
-                    foreach($section as $key => $value){
-                        $sect = new KeyValue([
-                            'form_id' => $json_body->form_id,
-                            'client_id' => $client->id,
-                            'key' => $key,
-                            'value' => $value,
-                            'description' => 0
+        try
+        {
+            $json_body = json_decode($request->getContent());
+            $client = null; 
+            if($json_body->client_id == null){
+            
+                foreach($json_body->sections as $section)
+                {
+                    if(!empty($section->document_type_id)){
+                        $client = new Client([
+                            'document_type_id' => $section->document_type_id,
+                            'first_name' => $section->firstName,
+                            'middle_name' => $section->middleName,
+                            'first_lastname' => $section->lastName,
+                            'second_lastname' => $section->secondLastName,
+                            'document' => $section->document
                         ]);
-                        $sect->save();
-                      
-                    }
-                }
-            }
-            $form_answer = new FormAnswer([
-                'user_id' => 1,
-                'channel_id' => 1,
-                'client_id' => $client->id,
-                'form_id' => $json_body->form_id,
-                'structure_answer' => json_encode($json_body->sections)
-            ]);
-            $form_answer->save();
-            return 'guardado';
-
-        }else{
-            $client = Client::find($json_body->client_id)->first();
-            $formanswer = FormAnswer::where( 'client_id', $json_body->client_id)->first();
-            $formanswer->structure_answer = json_encode($json_body->sections);
-            $formanswer->save();
-
-            foreach($json_body->sections as $section)
-            {
-                if(!empty($section->document_type_id)){
-
-                    $client->document_type_id = $section->document_type_id;
-                    $client->first_name = $section->firstName;
-                    $client->middle_name = $section->middleName;
-                    $client->first_lastname = $section->lastName;
-                    $client->second_lastname = $section->secondLastName;
-                    $client->document = $section->document;
-                    $client->save();
+                        $client->save();
+                    }else{
                     
-                }/* else{
-                    foreach($section as $key => $value){
-                        $sect->form_id = $json_body->form_id;
-                        $sect->client_id = $client->id;
-                        $sect->key = $key;
-                        $sect->value = $value;
-                        
-    
-                        $sect->save();
-                      
+                        foreach($section as $key => $value){
+                            $sect = new KeyValue([
+                                'form_id' => $json_body->form_id,
+                                'client_id' => $client->id,
+                                'key' => $key,
+                                'value' => $value,
+                                'description' => 0
+                            ]);
+                            $sect->save();
+                        }
                     }
                 }
+                $form_answer = new FormAnswer([
+                    'user_id' => 1,
+                    'channel_id' => 1,
+                    'client_id' => $client->id,
+                    'form_id' => $json_body->form_id,
+                    'structure_answer' => json_encode($json_body->sections)
+                ]);
+                $form_answer->save();
+                $message = 'Informacion guardada correctamente';
+                
+                
+            }else{
+                $message = '';
+                $client  = Client::find($json_body->client_id)->first();
+                $sect    = KeyValue::where('client_id', $json_body->client_id)->get();
+                if ($sect != null) {                      
+                    foreach($json_body->sections as $section)
+                    {   
+                        foreach($section as $key => $value){
+                            $where = [
+                                'client_id' => $json_body->client_id,
+                                'form_id' => $json_body->form_id,
+                                'key' => $key
+                            ];
+                            $keyValue = KeyValue::where($where)->first();
+                            if (!empty($keyValue) && is_object($keyValue)) {
+                                $keyValue->key = $key;
+                                $keyValue->value = $value;
+                                $keyValue->save();
+                            }
+                        }
+                        
+                        if(!empty($section->document_type_id)){
+                            $client->document_type_id = $section->document_type_id;
+                            $client->first_name = $section->firstName;
+                            $client->middle_name = $section->middleName;
+                            $client->first_lastname = $section->lastName;
+                            $client->second_lastname = $section->secondLastName;
+                            $client->document = $section->document;
+                            $client->save();
+                        }
+                    }
+                    $form_answer = new FormAnswer([
+                        'user_id' => 1,
+                        'channel_id' => 1,
+                        'client_id' => $client->id,
+                        'form_id' => $json_body->form_id,
+                        'structure_answer' => json_encode($json_body->sections),
+                    ]);
+                    $form_answer->save();
+                    $message = 'Informacion actualizada correctamente';
+                } else {
+                    foreach($json_body->sections as $section){
+                        foreach($section as $key => $value){
+                            $sect = new KeyValue([
+                                'form_id' => $json_body->form_id,
+                                'client_id' => $client->id,
+                                'key' => $key,
+                                'value' => $value,
+                                'description' => 0
+                            ]);
+                            $sect->save();
+                        }
+                    }
+                    
+                    $form_answer = new FormAnswer([
+                        'user_id' => 1,
+                        'channel_id' => 1,
+                        'client_id' => $client->id,
+                        'form_id' => $json_body->form_id,
+                        'structure_answer' => json_encode($json_body->sections),
+                    ]);
+                    $form_answer->save();
+                    $message = 'Informacion creada y actualizada correctamente';
+                }
+            
             }
-
-            return 'editado';
-        } */
-     
-    }
+            return $this->successResponse($message);
+    
+        }catch(\Throwable $e){
+            return $this->errorResponse('Error al guardar el formulario',500);
+        }
+}
 
       /**
      * Nicol Ramirez
@@ -101,12 +140,9 @@ class FormAnswerController extends Controller
      */
     public function filterForm(Request $request)
     {
-        $filter = DB::table('form_answers')
-                      ->join('clients','form_answers.client_id','=','clients.id')
-                      ->where('clients.document','like','%123456789%')
-                      ->get();
+     /*    $form_answer = FormAnswer::
 
-        return $filter;
+        return $filter; */
     }
 
      /**
