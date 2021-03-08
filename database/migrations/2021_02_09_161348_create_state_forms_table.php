5f518095b239a92ba558fcd637093b87043b3a5e
nicoll.ramirez <nicoll.r@montechelo.com.co>
2021-03-08 17:30:59 -0500

    ajustes

--- database/migrations/2021_02_09_161348_create_state_forms_table.php
+++ database/migrations/2021_02_09_161348_create_state_forms_table.php
@@ -16,7 +16,7 @@ class CreateStateFormsTable extends Migration
         Schema::create('state_forms', function (Blueprint $table) {
             $table->id();
             $table->string('name');
-            $table->integer('permissions');
+            $table->string('permissions');
             $table->boolean('approval');
             $table->text('observation');
             $table->datetime('date_update');
