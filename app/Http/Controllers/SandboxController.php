ba8f1191e3a249fdfd4f40fa530a78268f54e423
Carlos Galindez <cgalindez12@gmail.com>
2021-06-22 14:40:20 -0500

    nuevo servicio y funcionalidad para integracion SBS

---
+++ app/Http/Controllers/SandboxController.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Services\DataCRMService;
+class SandboxController extends Controller
+{
+    private $dataCrmService;
+    public function __construct(DataCRMService $dataCrmService)
+    {
+        $this->dataCrmService = $dataCrmService;
+    }
+    public function getContactsFromDataCRM(){
+        $this->dataCrmService->getContacts(1);
+    }
+}
