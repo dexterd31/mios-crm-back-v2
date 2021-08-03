851f624ba8d06a678f8a2b0d45e4304c57504b47
joao.b <joao.b@montechelo.com.co>
2021-08-03 14:44:52 -0500

    Listando Los modulos CRM

---
+++ app/Http/Controllers/ModuleCrmController.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\ModuleCrm;
+
+class ModuleCrmController extends Controller
+{
+    private $moduleCrmModel;
+
+    public function setModuleCrmModel($moduleCrmModel)
+	{
+		$this->moduleCrmModel = $moduleCrmModel;
+	}
+
+    public function getModuleCrmModel()
+	{
+		if($this->moduleCrmModel == null)
+		{
+			$this->setModuleCrmModel(new ModuleCrm());
+		}
+		return $this->moduleCrmModel;
+	}
+
+    public function store(Request $request)
+    {
+        $moduleCrmModel = $this->getModuleCrmModel();
+        return $moduleCrmModel->select("id", "label")->get();
+    }
+}
