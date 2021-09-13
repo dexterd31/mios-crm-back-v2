80afffd797cffa1ae2c5d6a5091b7bc88205bc4e
joao.b <joao.b@montechelo.com.co>
2021-09-13 11:10:26 -0500

    no message

--- database/seeds/FieldsClientUniqueIdentificatorSeeder.php
+++ database/seeds/FieldsClientUniqueIdentificatorSeeder.php
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 <?php
 
 use Illuminate\Database\Seeder;
@@ -61,43 +60,3 @@ class FieldsClientUniqueIdentificatorSeeder extends Seeder
         }
     }
 }
-=======
-<?php
-
-use Illuminate\Database\Seeder;
-use App\Models\Form;
-
-class FieldsClientUniqueIdentificatorSeeder extends Seeder
-{
-    /**
-     * Run the database seeds.
-     *
-     * @return void
-     */
-    public function run()
-    {
-        $forms = Form::all();
-        foreach ($forms as $form)
-        {
-            foreach ($form->sections as $section)
-            {
-                if($section->type_section == 1)
-                {
-                    $fields = json_decode($section->fields);
-                    foreach ($fields as $field)
-                    {
-                        $field->isClientInfo = true;
-                        if($field->key == "document")
-                        {
-                            $form->fields_client_unique_identificator = json_encode($field);
-                            break;
-                        }
-                    }
-                    break;
-                }
-            }
-            $form->save();
-        }
-    }
-}
->>>>>>> 9a01ced58d63a4a159d6e5c0d41a9969b8236399
