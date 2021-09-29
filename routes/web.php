cb64bee1c255937307e2f301da929557917d98b9
joao.b <joao.b@montechelo.com.co>
2021-09-29 16:01:08 -0500

    Creando end point para crear  clientes

--- routes/web.php
+++ routes/web.php
@@ -29,6 +29,7 @@ $router->group(['prefix' => 'api'], function () use ($router) {
     $router->post('/report','FormController@report');
     $router->get('/formsbyuser/{idUser}', 'FormController@formsByUser');
     $router->post('/addSection', 'FormController@addSection');
+    $router->get('/getDataClientInForm/{idForm}', 'FormController@getDataClientInForm');
 
     //Base de datos
     // la variable parameters esta en base64 y puede contener el caracter '/', lo cual lanza error 404,
