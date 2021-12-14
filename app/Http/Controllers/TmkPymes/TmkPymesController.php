05995aa2329b48355f8e3c8d173b228ee376b0fc
Leonardo Giraldo Quintero <giraldoquinteroleo@gmail.com>
2021-12-14 06:50:55 -0500

    Arreglo TMK
    En el elemento razon social ahora llega el ID separado por un asterisco y la razon social (id*razon_social)

--- app/Http/Controllers/TmkPymes/TmkPymesController.php
+++ app/Http/Controllers/TmkPymes/TmkPymesController.php
@@ -32,11 +32,12 @@ class TmkPymesController extends Controller
         $this->idFieldsInFormLead=(Object)[
             'nombres' => 1635436624162,
             'apellidos' => 9935436624162,
-            'razon_social' => 1635436930539,
+            'razon_social' => 1635437152258,
             'telefono' => 1635436912538,
             'utm_campaign' => 1635518784366,
             'tipo_documento' => 1637160407586,
-            'numero_documento' => 1635549672268
+            'numero_documento' => 1635549672268,
+            'id' => 1635436930539
         ];
         $this->productVicidial="TMK";
         $this->tokenVicidial="TmK202111031233";
@@ -85,7 +86,15 @@ class TmkPymesController extends Controller
                 if(isset($identification[1]))$this->leadTMKModify['tipo_documento']=$identification[0];
                 if(isset($identification[1]))$this->leadTMKModify['numero_documento']=$identification[1];
             }
-            unset($this->leadTMKModify['email']);
+            if(isset($this->leadTMKModify['razon_social'])){
+                $idRazonSocial=explode("*",$this->leadTMKModify['razon_social']);
+                if(isset($idRazonSocial[1])){
+                    $this->leadTMKModify['razon_social']=$idRazonSocial[1];
+                    $this->leadTMKModify['id']=$idRazonSocial[0];
+                }else{
+                    $this->leadTMKModify['id']=$this->leadTMKModify['razon_social'];
+                }
+            }
             if ($validator->fails()) return $this->responseTmk(implode(", ", $validator->errors()->all()), -1);
             $acount=(Object)[];
             $acount=$this->setAccount($this->formId,$this->setFieldToFillIn());
