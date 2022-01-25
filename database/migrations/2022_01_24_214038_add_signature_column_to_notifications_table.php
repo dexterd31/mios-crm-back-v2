d31b24aefa94b3403fbef2941e0846401857c3fb
Edwin David Sanchez Balbin <e.sanchez@montechelo.com.co>
2022-01-25 09:15:39 -0500

    Cambio en migracion para agregar la columna de firma de la notificación.

--- database/migrations/2022_01_24_214038_add_signature_column_to_notifications_table.php
+++ database/migrations/2022_01_24_214038_add_signature_column_to_notifications_table.php
@@ -14,7 +14,7 @@ class AddSignatureColumnToNotificationsTable extends Migration
     public function up()
     {
         Schema::table('notifications', function (Blueprint $table) {
-            $table->string('signature')->nullable()->default('default')->comment('Firma de la notificación.');
+            $table->string('signature')->nullable()->comment('Firma de la notificación.');
         });
     }
 
