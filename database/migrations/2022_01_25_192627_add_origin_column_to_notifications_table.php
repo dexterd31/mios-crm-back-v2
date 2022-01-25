3332416b22eae3332e5c9d4903d901fa2be37f4a
Edwin David Sanchez Balbin <e.sanchez@montechelo.com.co>
2022-01-25 14:55:56 -0500

    Se agrega columna de origen de la notificacion y se gerg

---
+++ database/migrations/2022_01_25_192627_add_origin_column_to_notifications_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class AddOriginColumnToNotificationsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('notifications', function (Blueprint $table) {
+            $table->string('origin', 100)->comment('Origen de donde sera enviada la notificación.');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('notifications', function (Blueprint $table) {
+            $table->dropColumn('origin');
+        });
+    }
+}
