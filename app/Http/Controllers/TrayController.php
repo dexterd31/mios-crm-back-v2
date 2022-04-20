8a8b56455ec8e8c0c05c51e13956ac5c1cae93f4
Edwin David Sanchez Balbin <e.sanchez@montechelo.com.co>
2022-04-20 16:12:35 -0500

    Ajuste.

--- app/Http/Controllers/TrayController.php
+++ app/Http/Controllers/TrayController.php
@@ -146,8 +146,8 @@ class TrayController extends Controller
         $sought = strtolower($request->sought);
         $filteredFields = $request->filteredFields ?? [];
         $columnToSort = $request->columnToSort;
-        $orientation = isset($request->orientation) ? $request->orientation : 'ASC';
-        $orientation = $orientation == '' || is_null($orientation) ? 'ASC' : $orientation;
+        $orientation = isset($request->orientation) ? $request->orientation : 'DESC';
+        $orientation = $orientation == '' || is_null($orientation) ? 'DESC' : $orientation;
         $tray = Tray::where('id',$id)->firstOrFail();
         $fieldsTable = collect(json_decode($tray->fields_table));
 
