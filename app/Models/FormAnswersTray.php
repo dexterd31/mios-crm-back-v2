a9ed9e63cee53cbed0362c80094c732c87ec8edd
Edwin David Sanchez Balbin <e.sanchez@montechelo.com.co>
2022-05-04 13:46:43 -0500

    Ajuste para la actualización de registros en bandejas.

--- app/Models/FormAnswersTray.php
+++ app/Models/FormAnswersTray.php
@@ -3,14 +3,11 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
-use App\Models\FormAnswer;
-use App\Models\Tray;
 
 class FormAnswersTray extends Model
-{
+{ 
     protected $table = 'form_answers_trays';
-    protected $PrimaryKey = 'id';
-    protected $fillable = ["id", "form_answer_id", "tray_id"];
+    protected $fillable = ["form_answer_id", "tray_id"];
 
     public function formAnswers(){
         return $this->belongsTo(FormAnswer::class, 'form_answer_id');
